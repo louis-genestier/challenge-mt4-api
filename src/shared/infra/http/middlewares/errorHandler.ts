4ba3d49986142fcf86b65d260bfd853ed3726a87
Louis Genestier <louis.genestier@gmail.com>
2022-07-12 19:52:38 +0200

    fix(errors): update errorHandler to handle non ApiError errors

--- src/shared/infra/http/middlewares/errorHandler.ts
+++ src/shared/infra/http/middlewares/errorHandler.ts
@@ -1,13 +1,16 @@
 import { NextFunction, Request, Response } from 'express'
 
 import { ApiError } from 'shared/infra/http/errors/apiError'
+import { HttpErrorCode } from 'shared/infra/http/errors/httpErrorCode'
 
 export const errorHandler = (
-  err: ApiError,
+  err: Error | ApiError,
   req: Request,
   res: Response,
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   next: NextFunction,
 ) => {
-  return res.status(err.httpCode).json(err.json)
+  if (err instanceof ApiError)
+    return res.status(err.httpCode).json(err.json)
+  return res.status(HttpErrorCode.InternalError).json(err.message)
 }
